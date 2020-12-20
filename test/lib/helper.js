352819378f5b6b501a476f91127d9e807d0d16e9
mwakizaka <mwakizaka0108@gmail.com>
2020-12-20 16:18:19 +0900

    Add: supports(?) iOS14 on m1 mac

--- test/lib/helper.js
+++ test/lib/helper.js
@@ -1,3 +1,6 @@
+import compareVersions from 'compare-versions';
+
+
 function setNativeValue(element, value) {
   const { set: valueSetter } = Object.getOwnPropertyDescriptor(element, "value") || {};
   const prototype = Object.getPrototypeOf(element);
@@ -13,7 +16,7 @@ function setNativeValue(element, value) {
   element.dispatchEvent(new Event("input", { bubbles: true }));
 }
 
-function customCommand(value) {
+function addSetNativeValueCommand(value) {
   if (driver.isIOS) {
     browser.execute(setNativeValue, this, value);
   } else {
@@ -21,6 +24,17 @@ function customCommand(value) {
   }
 }
 
+function addClickForIOSSafari14Command(value) {
+  if (driver.isIOS && compareVersions(driver.capabilities['safari:platformVersion'], '13.*') > 0) {
+    browser.execute("arguments[0].click(); arguments[0].dispatchEvent(new Event('click', { bubbles: true }));", this);
+  } else {
+    this.click();
+  }
+}
+
+
+
 module.exports = {
-  customCommand: customCommand
+  addSetNativeValueCommand: addSetNativeValueCommand,
+  addClickForIOSSafari14Command: addClickForIOSSafari14Command
 }
